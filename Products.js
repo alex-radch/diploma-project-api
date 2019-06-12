ee43c7b51fad0f942e9734330f0d42c05375321b
Mikhail Radchenko <mradchenko@provenworks.com>
2019-06-12 22:43:30 +0300

    added api for products

---
+++ Products.js
@@ -0,0 +1,77 @@
+const products = [
+  {
+    id: 1,
+    src: 'img/outer7.jpg',
+    type: 'Outer',
+    title: 'Weds 17"',
+    cost: 140
+  },  {
+    id: 11,
+    src: 'img/outer6.jpg',
+    type: 'Outer',
+    title: 'Rays 18"',
+    cost: 150
+  },{
+    id: 3,
+    src: 'img/outer7.jpg',
+    type: 'Outer',
+    title: 'Weds 19"',
+    cost: 160
+  },  {
+    id: 5,
+    src: 'img/outer5.jpg',
+    type: 'Outer',
+    title: 'Work 18"',
+    cost: 155
+  }, {
+    id: 9,
+    src: 'img/outer3.jpg',
+    type: 'Outer',
+    title: 'SSR 18"',
+    cost: 150
+  },{
+    id: 6,
+    src: 'img/outer5.jpg',
+    type: 'Outer',
+    title: 'Work 19"',
+    cost: 165
+  }, {
+    id: 7,
+    src: 'img/outer1.jpg',
+    type: 'Outer',
+    title: 'BBS 15"',
+    cost: 120
+  }, {
+    id: 4,
+    src: 'img/outer5.jpg',
+    type: 'Outer',
+    title: 'Work 17"',
+    cost: 145
+  },{
+    id: 8,
+    src: 'img/outer2.jpg',
+    type: 'Outer',
+    title: 'BBS 16"',
+    cost: 130
+  },  {
+    id: 10,
+    src: 'img/outer4.jpg',
+    type: 'Outer',
+    title: 'Leon 19"',
+    cost: 170
+  }, {
+    id: 2,
+    src: 'img/outer6.jpg',
+    type: 'Outer',
+    title: 'Weds 18"',
+    cost: 150
+  },{
+    id: 12,
+    src: 'img/outer7.jpg',
+    type: 'Outer',
+    title: 'Piaa 18"',
+    cost: 160
+}];
+  
+module.exports = products;
+  
