02bf8ee41087ab33dc624b1384d5907eb33d8a85
Mikhail Radchenko <mradchenko@provenworks.com>
2019-06-13 06:36:29 +0300

    merged with react

---
+++ public/precache-manifest.6d9f2af4a648ac27e6d6343b621ad31b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed048d4ffc56baadd1d5aaf652d17689",
+    "url": "/index.html"
+  },
+  {
+    "revision": "868bf54b25df35ef287a",
+    "url": "/static/css/main.6a613cfa.chunk.css"
+  },
+  {
+    "revision": "aae3871f07e128989724",
+    "url": "/static/js/2.2b1bb275.chunk.js"
+  },
+  {
+    "revision": "868bf54b25df35ef287a",
+    "url": "/static/js/main.e6f018e4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
